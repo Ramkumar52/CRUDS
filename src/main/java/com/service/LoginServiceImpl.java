47338d53917e47df0f221add1eac85b45b91d5c8
Ramkumar52 <kramkumar754@gmail.com>
2016-09-28 22:40:32 +0530

    New CRUD operation

---
+++ src/main/java/com/service/LoginServiceImpl.java
@@ -0,0 +1,28 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dao.loginDao;
+import com.model.User;
+
+@Service
+@Transactional
+public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	private loginDao ld;
+	
+	@Override
+	public boolean checkUser(User u) {
+		boolean b=false;
+		b=ld.checkUser(u);
+		if(b==true){
+			b=true;
+		}
+		return b;
+	}
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="login">login</a>
+${msg }
+${name }
+</body>
+</html>
